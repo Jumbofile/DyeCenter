4c40c24ca5d892e686de4e4bf4533ab786b4a1d5
jumbofile <gplachno@ycp.edu>
2019-11-16 17:00:32 -0500

    Update GameServlet.java

--- src/servlet/GameServlet.java
+++ src/servlet/GameServlet.java
@@ -104,77 +104,16 @@ public class GameServlet extends HttpServlet {
 
             String points = (String)req.getParameter("point");
             int playerBeingModified = Integer.parseInt(req.getParameter("player"));
-            if(points.equals("finish")){
-                //finish the game
-                try {
-                    ArrayList<String> gameStats = db.getGameStats(Integer.parseInt(gid));
-                    int t1Score = Integer.parseInt(gameStats.get(2)) ;
-                    int t2Score = Integer.parseInt(gameStats.get(3)) ;
+            String[] data1 = req.getAttribute("card-data-1").toString().split(",");
+            String[] data2 = req.getAttribute("card-data-2").toString().split(",");
+            String[] data3 = req.getAttribute("card-data-3").toString().split(",");
+            String[] data4 = req.getAttribute("card-data-4").toString().split(",");
 
-                    System.out.println("t1: " + t1Score + " t2:" + t2Score);
-                    //get players
-                    String[] t1Players = gameStats.get(0).split(",") ;
-                    String[] t2Players = gameStats.get(1).split(",") ;
-
-                    // UID values for team 1
-                    String t1p1UID = t1Players[0].split("~")[0];
-                    String t1p2UID = t1Players[1].split("~")[0];
-
-                    // UID values for team 2
-                    String t2p1UID = t2Players[0].split("~")[0];
-                    String t2p2UID = t2Players[1].split("~")[0];
-
-                    int[] team1 = new int[]{Integer.parseInt(t1p1UID), Integer.parseInt(t1p2UID)};
-                    int[] team2 = new int[]{Integer.parseInt(t2p1UID), Integer.parseInt(t2p2UID)};
-                    if(t1Score > t2Score) {
-                        System.out.println("team1 won");
-                        db.setWinners(team1, 1, Integer.parseInt(gid));
-                        db.setLosers(team2, 1, Integer.parseInt(gid));
-                    }else{
-                        System.out.println("team2 won");
-                        db.setWinners(team2, 2, Integer.parseInt(gid));
-                        db.setLosers(team1, 2, Integer.parseInt(gid));
-                    }
-                }catch(Exception e){
-
-                }
-
-            }else {
-                if (playerBeingModified  != -1) {
-                    System.out.println("point: " + points);
-                    System.out.println("player: " + player);
-                    System.out.println("Len: " + points.length());
-
-                    try {
+            //Game updatedGame = new Game()
+            //update player 1
+            Player player1 = new Player(data1[1]);
 
 
-                        if (playerBeingModified  <= 1) {
-                            if (points.charAt(points.length() - 1) == '~') {
-                                int onlyPoint = Integer.parseInt(points.substring(0, points.length() - 1));
-                                System.out.println("point: " + onlyPoint);
-                                db.updateUserPoints(onlyPoint, players[player]);
-                                db.updateGameScore(onlyPoint, 1, Integer.parseInt(gid), players[player]);
-                                db.updateUserPlunks(players[player], 0);
-                            } else {
-                                db.updateGameScore(Integer.parseInt(points), 1, Integer.parseInt(gid), players[player]);
-                                db.updateUserPoints(Integer.parseInt(points), players[player]);
-                            }
-                        } else {
-                            if (points.charAt(points.length() - 1) == '~') {
-                                int onlyPoint = Integer.parseInt(points.substring(0, points.length() - 1));
-                                db.updateUserPoints(onlyPoint, players[player]);
-                                db.updateGameScore(onlyPoint, 1, Integer.parseInt(gid), players[player]);
-                                db.updateUserPlunks(players[player], 1);
-                            } else {
-                                db.updateGameScore(Integer.parseInt(points), 2, Integer.parseInt(gid), players[player]);
-                                db.updateUserPoints(Integer.parseInt(points), players[player]);
-                            }
-                        }
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
             }
             try {
                 resp.sendRedirect(req.getContextPath() + "/game");
